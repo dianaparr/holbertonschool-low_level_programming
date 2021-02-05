bf6937743ec539ffa6597a0a8c8891aee8b23143
dianaparr <dianaparra.9611@gmail.com>
2021-02-05 04:35:37 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	for (num = 'a'; num <= 'f'; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
