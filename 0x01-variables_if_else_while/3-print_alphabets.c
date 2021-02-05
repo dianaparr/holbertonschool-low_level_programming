0dc41acb866e4121fa47dab9fb79bc01d3bcf107
dianaparr <dianaparra.9611@gmail.com>
2021-02-05 02:39:05 +0000

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char ap;
+
+	for (ap = 'a'; ap <= 'z'; ap++)
+	{
+		putchar(ap);
+	}
+	for (ap = 'A'; ap <= 'Z'; ap++)
+	{
+		putchar(ap);
+	}
+	return (0);
+}
