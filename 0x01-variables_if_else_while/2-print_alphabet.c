58f6f638b52830a59cdb28e3806909fbb13d3f12
dianaparr <dianaparra.9611@gmail.com>
2021-02-05 02:26:18 +0000

    Alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char ap;
+
+	for (ap = 'a'; ap <= 'z'; ap++)
+	{
+		putchar(ap);
+	}
+	return (0);
+}
