d284a353c064e0fd5a2c62ec6228ef187c636496
dianaparr <dianaparra.9611gmail.com>
2021-02-09 07:00:27 -0500

    prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet: Entry point
+ *
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0
+ *
+ **/
+
+void print_alphabet(void)
+{
+	char alpha;
+	
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+}
