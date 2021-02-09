9e1043a27907b81d5ab1c8f552da268295f383dd
dianaparr <dianaparra.9611gmail.com>
2021-02-09 11:37:06 -0500

    prints the sign of a number_modificated1

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,9 @@
 #include "holberton.h"
 
 /**
- *
- * print_sign - prints the sign of a number 
- *
- * Return: 0 always
- *
+ *print_sign - prints the sign of a number
+ *@n: The character print
+ *Return: 0 always
  **/
 
 int print_sign(int n)
