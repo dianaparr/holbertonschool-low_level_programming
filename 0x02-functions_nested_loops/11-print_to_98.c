576202df42a859f811b366fef494028f509c015d
dianaparr <dianaparra.9611gmail.com>
2021-02-10 13:00:01 -0500

    print all natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *print_to_98 - print natural numbers
+ *@n: natural number inicialization
+ *Return: 0 always
+ **/
+
+void print_to_98(int n)
+{
+	int nat;
+
+	nat = n;
+	while (nat < 99)
+	{
+		printf("%d, ", nat);
+		nat++;
+	}
+	if (nat <= 98)
+	{
+		printf("%d", nat);
+	}
+}
