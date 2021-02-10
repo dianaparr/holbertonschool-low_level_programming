1e438962d0dcf141515aa9aa5bd149258ec1c557
dianaparr <dianaparra.9611gmail.com>
2021-02-10 12:05:28 -0500

    sum two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *add - prints add two numbers
+ *@r: number one
+ *@s: number two
+ *Return: 0 always
+ **/
+
+int add(int r, int s)
+{
+	int t;
+
+	t = r + s;
+	return (t);
+
+}
