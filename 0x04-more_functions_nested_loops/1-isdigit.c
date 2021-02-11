4f37c78aea3c25ea104613aa1fde29ed3c607516
dianaparr <dianaparra.9611gmail.com>
2021-02-11 12:45:38 -0500

    chacks for a digit (0-9)

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *_isdigit - function checks for a digit
+ *@c: The character print
+ *Return: 0 always
+**/
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c >= 57)
+	{
+		return (1);
+	}
+	else if (c >= 97 && c >= 122)
+	{
+		return (0);
+	}
+	return (0);
+}
