18904493ff5c396a7791689aaaba1909cea7488c
dianaparr <dianaparra.9611@gmail.com>
2021-02-18 21:46:09 -0500

    Function that compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *_strcmp - compare two strings
+ *@s1: string one
+ *@s2: string two
+ *Return: Always 0.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	if (s1 == s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (s1 - s2);
+	}
+	
+}
