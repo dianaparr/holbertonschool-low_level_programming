3ed38ab605b7990e1ff731a23674bfcaa9aae9be
dianaparr <dianaparra.9611@gmail.com>
2021-02-28 20:15:26 -0500

    Header file_all prototypes_project of static libraries_modificated macros2

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,6 +1,6 @@
 /* All prototypes of the functions */
-#ifndef HEADER_HOLBERTON
-#define HEADER_HOLBERTON
+#ifndef HEADER_H
+#define HEADER_H
 
 int _putchar(char c);
 int _islower(int c);
@@ -23,4 +23,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* HEADER_H */
