52b5b01ec130d247c03495ce3786f314de087f08
dianaparr <dianaparra.9611@gmail.com>
2021-03-03 17:23:10 -0500

    Function that returns a pointer to a 2 dimensional array of integers_modificated betty

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 
 /**
- *alloc_grid - 
- *@width: 
- *@height: 
+ *alloc_grid - two dimensional array of integers
+ *@width: type int_the width of the two dimensional array
+ *@height: type int_the height of the two dimensional array
  *Return: pointer to str or NULL (if it fails)
  */
 
@@ -13,31 +13,31 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int h;
 
-	if (width <= 0 || height <= 0)   /* If width or height is 0 or negative, return NULL */
+	if (width <= 0 || height <= 0)   /* width or height is 0 or negative */
 	{
 		return (NULL);
 	}
 	grid = malloc(height * sizeof(int *));
-	if (grid == NULL)
+	if (grid == NULL)            /* memory allocation check */
 	{
 		return (NULL);
 	}
-	for (i = 0; i < height; i++)   /* memory */
+	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(width * sizeof(int));
-		if (grid[i] == NULL)
+		if (grid[i] == NULL)       /* memory allocation check */
 		{
 			for (h = i; h >= 0; h--)
 			{
-				free (grid[h]);
+				free(grid[h]);
 			}
-			free (grid);
+			free(grid);
 			return (NULL);
 		}
 	}
-	for (i = 0; i < height; i++)   /* valores */
+	for (i = 0; i < height; i++)   /* prints values of the grid */
 	{
-		for (j = 0; j < width; j ++)
+		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
 		}
