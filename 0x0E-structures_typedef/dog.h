b93d34fb9cb8d66fcd5031e998f11baa16bc6a07
dianaparr <dianaparra.9611@gmail.com>
2021-03-08 16:11:06 -0500

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+/**
+ * struct dog - name of the structure
+ * @name: pointer type char
+ * @owner: pointer type char
+ * @age: type float
+ *
+ * Description: structure type with the elements name, owner and age
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+
+#endif /* HEADER_H */
