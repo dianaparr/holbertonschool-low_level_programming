803b89d837c95bd884ed321c16d0ed757f988f54
dianaparr <dianaparra.9611@gmail.com>
2021-03-08 23:29:40 -0500

    Define a new type struct dog_with prototypes_update typedef1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,8 +17,9 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
+void free_dog(dog_t *d);
 
 #endif /* HEADER_H */
