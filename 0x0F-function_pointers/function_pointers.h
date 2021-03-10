ae57796f5c8b2d3da66433a186d718d067f99f62
dianaparr <dianaparra.9611@gmail.com>
2021-03-10 16:45:10 -0500

    Header file_all prototypes of functions_update1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
-#include <stdlib.h>
-#include <stddef.h>
 #ifndef HEADER_H
 #define HEADER_H
+#include <stdlib.h>
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
