2f755c0a23c13871004f062f0f2923bc6d23612f
dianaparr <dianaparra.9611@gmail.com>
2021-03-19 09:47:24 -0500

    Headere file_all prototypes and structure_update with library

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -25,5 +26,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+int len_str(char *s);
 
 #endif /* HEADER_H */
