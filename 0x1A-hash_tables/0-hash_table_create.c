2ef9b193a42f51e7ae6998b0a064c230abe9d995
dianaparr <dianaparra.9611@gmail.com>
2021-05-27 18:23:42 -0500

    1024 bytes are being allocated

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,13 +11,14 @@ hash_table_t *hash_table_create(unsigned long int size)
     unsigned long int index = 0;
 
     /*allocated memory*/
-    new_table = malloc(size * sizeof(hash_table_t));
+    new_table = malloc(sizeof(hash_table_t));
     if (new_table == NULL)
         return (NULL);
-
+    new_table->size = size;
+    new_table->array = malloc(size * sizeof(hash_node_t *));
     for (; index < size; index++)
     {
-        new_table[index].array = NULL; /*head in each position of the array*/
+        new_table->array[index] = NULL; /*head in each position of the array*/
     }
     return (new_table);
 }
