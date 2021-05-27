e30314fd728ce8cf120470d522f17bc91f2b64e3
dianaparr <dianaparra.9611@gmail.com>
2021-05-27 17:01:44 -0500

    actualization in loop

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,13 +11,13 @@ hash_table_t *hash_table_create(unsigned long int size)
     unsigned long int index = 0;
 
     /*allocated memory*/
-    new_table = malloc(size * sizeof(hash_table_t*));
+    new_table = malloc(size * sizeof(hash_table_t));
     if (new_table == NULL)
         return (NULL);
 
-    for (; index < new_table->size; index++)
+    for (; index < size; index++)
     {
-        new_table->array[index] = NULL; /*head in each position of the array*/
+        new_table[index].array = NULL; /*head in each position of the array*/
     }
     return (new_table);
 }
