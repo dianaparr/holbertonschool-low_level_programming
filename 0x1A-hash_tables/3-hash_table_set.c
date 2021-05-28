22a81c859d744d2b549d873a0aaf3c05bbbe90ea
dianaparr <dianaparra.9611@gmail.com>
2021-05-27 22:10:00 -0500

    try, add new node in the begin to the list

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,28 @@
 #include "hash_tables.h"
 
 /**
+ * add_node_begin - 
+ * @head: 
+ * @key: 
+ * @size: 
+ * Return: 
+ */
+hash_node_t *add_node_begin(hash_node_t *head, const char *key, const char *value)
+{
+	hash_node_t *new_node_begin = NULL;
+
+	new_node_begin = malloc(sizeof(hash_node_t));
+    if (new_node_begin == NULL)
+        return (NULL);
+    new_node_begin->key = strdup(key);
+    new_node_begin->value = strdup(value);
+    new_node_begin->next = head;
+    head = new_node_begin;
+    return (head);
+}
+
+
+/**
  * hash_table_set - 
  * @key: 
  * @size: 
@@ -14,11 +36,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
     /*key can not be an empty string*/
     if (strlen(key) == 0)
         return (0);
+    /*compute index (hash value of key)*/
+    index = key_index((const unsigned char *)key, ht->size);
+    /* add new node a beginning list = resolve colision*/
     
-    new_element = malloc(sizeof(hash_node_t));
-    if (new_element == NULL)
-        return (NULL);
-    new_element->key = strdup(key);
-    new_element->value = strdup(value);
+    
     
 }
