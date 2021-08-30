def2387774c59ef6a2c0bd5c581c2192050423d7
dianaparr <dianaparra.9611@gmail.com>
2021-08-30 12:52:02 -0500

    Uploading first version to the task

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,64 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - search for a value in an sorted array of integers
+ * @array: pointer to the first element of the array
+ * @size: number elements of the array
+ * @value: value to search for
+ *
+ * The array will be sorted in ascending order
+ * The value won't appear more than once in array
+ *
+ * Return: the first value index where value is located
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int left_elm = 0;
+	int right_elm = size - 1;
+	int middle_elm = 0;
+
+	if (value == '\0' || array == NULL)
+		return (-1);
+
+	/* If L > R, the search terminates as unsuccessful */
+	while (left_elm < right_elm)
+	{
+		middle_elm = (left_elm + right_elm) / 2;
+		print_array(array, right_elm, left_elm);
+		/* Begins by comparing an element in the middle of the array */
+		/* with the target value. */
+		if (array[middle_elm] == value)
+			return (middle_elm);
+		/* Two cases to search with relation to one target value and position in the array */
+		else if (array[middle_elm] < value)
+			left_elm = middle_elm + 1;
+		else
+			right_elm = middle_elm - 1;
+	}
+	printf("Searching in array: %d\n", array[right_elm]);
+	if (array[right_elm] == value)
+		return (right_elm);
+
+	return (-1);
+}
+
+/**
+ * print_array - for print elements of the array
+ * @array: pointer to the first element of the array
+ * @
+ * @
+ *
+ * Return: the first value index where value is located
+ */
+void print_array(int *array, int right_elm, int left_elm)
+{
+	int i = left_elm;
+
+	printf("Searching in array: ");
+	while (i < right_elm)
+	{
+		printf("%d, ", array[i]);
+		i++;
+	}
+	printf("%d\n", array[i]);
+}
