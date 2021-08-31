e05737706ede6ef81b2a75abb06449449493206d
dianaparr <dianaparra.9611@gmail.com>
2021-08-31 10:02:58 -0500

    Uploading task 102 about algorith interpolation search

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,47 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - search for a value in an sorted array of integers
+ * @array: pointer to the first element of the array
+ * @size: number elements of the array
+ * @value: value to search for
+ *
+ * To determine the probe position, you can use : size_t pos =
+ * low + (((double)(high - low) / (array[high] - array[low])) *
+ * (value - array[low]))
+ *
+ * Return: the first value index where value is located
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	unsigned int low = 0, high = size - 1;
+
+	if (array == NULL)
+		return (-1);
+
+	size_t pos = low + (((double)(high - low) / (array[high] - array[low]))
+	* (value - array[low]));
+
+	while (pos <= size)
+	{
+		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+		if (array[pos] == value)
+		{
+			return (pos);
+		}
+		else if (array[pos] < value)
+		{
+			low = pos + 1;
+			pos = low + (((double)(high - low) / (array[high] - array[low]))
+			* (value - array[low]));
+		}
+		else
+		{
+			high = pos - 1;
+			pos = low + (((double)(high - low) / (array[high] - array[low]))
+			* (value - array[low]));
+		}
+	}
+	printf("Value checked array[%ld] is out of range\n", pos);
+	return (-1);
+}
