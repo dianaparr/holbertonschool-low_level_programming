76084817f88f75a60ecd83b74583d7b6056fade3
dianaparr <dianaparra.9611@gmail.com>
2021-08-31 17:38:28 -0500

    Update header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -15,5 +15,7 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int binary_search_version2(int *array, int lower, int higher, int value);
+int advanced_binary(int *array, size_t size, int value);
+int advanced_binary_recursion(int *array, int lower, int higher, int value);
 
 #endif /* HEADER_H */
