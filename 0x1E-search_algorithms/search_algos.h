48999b38da76ca5652ca342cdbb4a4f4d237b4c9
dianaparr <dianaparra.9611@gmail.com>
2021-08-31 13:40:29 -0500

    Uploading header file update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,9 +3,17 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
+/* mandatory tasks */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, int right_elm, int left_elm);
+
+/* advanced tasks */
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int binary_search_version2(int *array, int lower, int higher, int value);
 
 #endif /* HEADER_H */
