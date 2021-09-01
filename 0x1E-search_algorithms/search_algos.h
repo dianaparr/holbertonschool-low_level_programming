c01eef0ab17de856c78329c10c760d4295a08ec9
dianaparr <dianaparra.9611@gmail.com>
2021-08-31 22:13:58 -0500

    Update header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,24 @@
 #include <stdio.h>
 #include <math.h>
 
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
 /* mandatory tasks */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
@@ -17,5 +35,6 @@ int exponential_search(int *array, size_t size, int value);
 int binary_search_version2(int *array, int lower, int higher, int value);
 int advanced_binary(int *array, size_t size, int value);
 int advanced_binary_recursion(int *array, int lower, int higher, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 #endif /* HEADER_H */
